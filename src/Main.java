14404b2b0dfcd0d704af81d154ad6001b9b96040
João Carlos Santos <joaofloressantos@gmail.com>
2014-11-12 16:17:47 +0000

    Can now communicate with SUMO using TraSMAPI.

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import it.polito.appeal.traci.SumoTraciConnection;
+import trasmapi.genAPI.Simulator;
+import trasmapi.genAPI.TraSMAPI;
+import trasmapi.genAPI.exceptions.TimeoutException;
+import trasmapi.genAPI.exceptions.UnimplementedMethod;
+import trasmapi.sumo.Sumo;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Vinnie on 12-Nov-14.
+ */
+public class Main {
+    public static void main(String[] args) throws UnimplementedMethod, IOException, TimeoutException,
+            InterruptedException {
+        //TRASMAPI STUFF
+        TraSMAPI api = new TraSMAPI();
+
+        // SUMO STUFF
+        Simulator sumo = new Sumo("guisim");
+        List<String> params = new ArrayList<String>();
+        params.add("-c=TlMap/map.sumo.cfg");
+        sumo.addParameters(params);
+        sumo.addConnections("localhost", 8870);
+
+
+        api.addSimulator(sumo);
+        api.launch();
+        api.connect();
+        api.start();
+
+        Thread.sleep(1000);
+        System.out.println("Chego.");
+    }
+}
---
+++ src/trasmapi/genAPI/Simulator.java
@@ -0,0 +1,65 @@
+package trasmapi.genAPI;
+
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.List;
+
+import trasmapi.genAPI.exceptions.TimeoutException;
+import trasmapi.genAPI.exceptions.UnimplementedMethod;
+
+public class Simulator {
+	
+	/**
+	 * launches the Simulator Process
+	 * @param params 
+	 * @throws java.io.IOException
+	 * @throws trasmapi.genAPI.exceptions.UnimplementedMethod
+	 */
+	public void launch() throws IOException, UnimplementedMethod {
+		throw new UnimplementedMethod();
+	}
+	
+	/**
+	 * method used to connect to the simulator (using sockets)
+	 * @throws java.net.UnknownHostException
+	 * @throws java.io.IOException
+	 * @throws trasmapi.genAPI.exceptions.TimeoutException
+	 */
+	public void connect() throws UnknownHostException, IOException, TimeoutException, UnimplementedMethod {
+		throw new UnimplementedMethod();
+	}
+	
+	/**
+	 * method used to close the simulator
+	 * @throws java.io.IOException
+	 */
+	public void close() throws UnimplementedMethod, IOException {
+		throw new UnimplementedMethod();
+	}
+	
+	/**
+	 * method used to advance k steps in the simulation
+	 * @param k - number of simulation steps to be simulated
+	 */
+	public boolean simulationStep(int k) throws UnimplementedMethod {
+		throw new UnimplementedMethod();
+	}
+
+	public void addParameters(List<String> params) throws UnimplementedMethod {
+		throw new UnimplementedMethod();
+	}
+
+	public void addConnections(String string, int i) throws UnimplementedMethod {
+		throw new UnimplementedMethod();
+		
+	}
+
+	public void getAllVehiclesIds() throws UnimplementedMethod {
+		throw new UnimplementedMethod();
+	}
+
+	public void start() throws UnimplementedMethod {
+		throw new UnimplementedMethod();
+	}
+}
