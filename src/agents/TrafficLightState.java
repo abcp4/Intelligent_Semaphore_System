d6ab7fbacf3fe39f2e74ed173a53ab8ae994013a
Diogo Pinto <diogojapinto@gmail.com>
2014-12-14 21:40:26 +0000

    added q-table print

--- src/agents/TrafficLightState.java
+++ src/agents/TrafficLightState.java
@@ -25,16 +25,15 @@ public class TrafficLightState implements State {
         lastAction = 0;
         this.nrIntersections = nrIntersections;
         greenTimeSpans = new int[nrIntersections];
-        if (!TrafficLightAgent.IS_FIXED_BEHAVIOUR) {
-            //updateState(new Random().nextInt(nrStates));
-            updateState(0);
-        } else {
+        /*if (!TrafficLightAgent.IS_FIXED_BEHAVIOUR) {
+            updateState(new Random().nextInt(nrStates));
+        } else {*/
             state = 0;
             for (int i = 0; i < nrIntersections; i++) {
                 state += Math.pow(NR_STATES_PER_LIGHT, i) * 2;
             }
             updateState(state);
-        }
+        //}
     }
 
     public int getGreenTimeSpan(int index) {
